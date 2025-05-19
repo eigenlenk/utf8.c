911efed93b6febe9bbad17f0cce371cb9f635018
Eigen Lenk <eigen.lenk@gmail.com>
2025-05-19 12:57:56 +0300

    Replace binary literals to satisfy '-pedantic'

--- utf8.c
+++ utf8.c
@@ -3,6 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define B00000000 0
+#define B00000010 2
+#define B00000111 7
+#define B00001111 15
+#define B00010000 16
+#define B00011111 31
+#define B00100000 32
+#define B00111111 63
+#define B01111111 127
+#define B10000000 128
+#define B10100000 160
+#define B10111111 191
+#define B11000000 192
+#define B11100000 224
+#define B11101101 237
+#define B11110000 240
+#define B11111000 248
+
 typedef struct {
     bool valid;
     size_t next_offset;
@@ -10,12 +28,12 @@ typedef struct {
 
 utf8_char_validity validate_utf8_char(const char* str, size_t offset) {
     // Single-byte UTF-8 characters have the form 0xxxxxxx
-    if (((uint8_t)str[offset] & 0b10000000) == 0b00000000)
+    if (((uint8_t)str[offset] & B10000000) == B00000000)
         return (utf8_char_validity) { .valid = true, .next_offset = offset + 1 };
 
     // Two-byte UTF-8 characters have the form 110xxxxx 10xxxxxx
-    if (((uint8_t)str[offset + 0] & 0b11100000) == 0b11000000 &&
-        ((uint8_t)str[offset + 1] & 0b11000000) == 0b10000000) {
+    if (((uint8_t)str[offset + 0] & B11100000) == B11000000 &&
+        ((uint8_t)str[offset + 1] & B11000000) == B10000000) {
 
         // Check for overlong encoding
         // 0(xxxxxxx)
@@ -23,16 +41,16 @@ utf8_char_validity validate_utf8_char(const char* str, size_t offset) {
         // 110(xxxxx) 10(xxxxxx)
         // 110(00001) 10(111111)
         // 110(00010) 10(000000)
-        if (((uint8_t)str[offset] & 0b00011111) < 0b00000010)
+        if (((uint8_t)str[offset] & B00011111) < B00000010)
             return (utf8_char_validity) { .valid = false, .next_offset = offset };
 
         return (utf8_char_validity) { .valid = true, .next_offset = offset + 2 };
     }
 
     // Three-byte UTF-8 characters have the form 1110xxxx 10xxxxxx 10xxxxxx
-    if (((uint8_t)str[offset + 0] & 0b11110000) == 0b11100000 &&
-        ((uint8_t)str[offset + 1] & 0b11000000) == 0b10000000 &&
-        ((uint8_t)str[offset + 2] & 0b11000000) == 0b10000000) {
+    if (((uint8_t)str[offset + 0] & B11110000) == B11100000 &&
+        ((uint8_t)str[offset + 1] & B11000000) == B10000000 &&
+        ((uint8_t)str[offset + 2] & B11000000) == B10000000) {
 
         // Check for overlong encoding
         // 110(xxxxx) 10(xxxxxx)
@@ -40,26 +58,26 @@ utf8_char_validity validate_utf8_char(const char* str, size_t offset) {
         // 1110(xxxx) 10(xxxxxx) 10(xxxxxx)
         // 1110(0000) 10(011111) 10(111111)
         // 1110(0000) 10(100000) 10(000000)
-        if (((uint8_t)str[offset + 0] & 0b00001111) == 0b00000000 &&
-            ((uint8_t)str[offset + 1] & 0b00111111) < 0b00100000)
+        if (((uint8_t)str[offset + 0] & B00001111) == B00000000 &&
+            ((uint8_t)str[offset + 1] & B00111111) < B00100000)
             return (utf8_char_validity) { .valid = false, .next_offset = offset };
 
         // Reject UTF-16 surrogates
         // U+D800 to U+DFFF
         // 1110(1101) 10(100000) 10(000000) ED A0 80 to 1110(1101) 10(111111) 10(111111) ED BF BF
-        if ((uint8_t)str[offset + 0] == 0b11101101 &&
-            (uint8_t)str[offset + 1] >= 0b10100000 &&
-            (uint8_t)str[offset + 1] <= 0b10111111)
+        if ((uint8_t)str[offset + 0] == B11101101 &&
+            (uint8_t)str[offset + 1] >= B10100000 &&
+            (uint8_t)str[offset + 1] <= B10111111)
             return (utf8_char_validity) { .valid = false, .next_offset = offset };
 
         return (utf8_char_validity) { .valid = true, .next_offset = offset + 3 };
     }
 
     // Four-byte UTF-8 characters have the form 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-    if (((uint8_t)str[offset + 0] & 0b11111000) == 0b11110000 &&
-        ((uint8_t)str[offset + 1] & 0b11000000) == 0b10000000 &&
-        ((uint8_t)str[offset + 2] & 0b11000000) == 0b10000000 &&
-        ((uint8_t)str[offset + 3] & 0b11000000) == 0b10000000) {
+    if (((uint8_t)str[offset + 0] & B11111000) == B11110000 &&
+        ((uint8_t)str[offset + 1] & B11000000) == B10000000 &&
+        ((uint8_t)str[offset + 2] & B11000000) == B10000000 &&
+        ((uint8_t)str[offset + 3] & B11000000) == B10000000) {
 
         // Check for overlong encoding
         // 1110(xxxx) 10(xxxxxx) 10(xxxxxx)
@@ -67,8 +85,8 @@ utf8_char_validity validate_utf8_char(const char* str, size_t offset) {
         // 11110(xxx) 10(xxxxxx) 10(xxxxxx) 10(xxxxxx)
         // 11110(000) 10(001111) 10(111111) 10(111111)
         // 11110(000) 10(010000) 10(000000) 10(000000)
-        if (((uint8_t)str[offset + 0] & 0b00000111) == 0b00000000 &&
-            ((uint8_t)str[offset + 1] & 0b00111111) < 0b00010000)
+        if (((uint8_t)str[offset + 0] & B00000111) == B00000000 &&
+            ((uint8_t)str[offset + 1] & B00111111) < B00010000)
             return (utf8_char_validity) { .valid = false, .next_offset = offset };
 
         return (utf8_char_validity) { .valid = true, .next_offset = offset + 4 };
@@ -129,9 +147,9 @@ owned_utf8_string make_utf8_string_lossy(const char* str) {
             //      = (1111) (111111) (111101)
             //      = 1110(1111) 10(111111) 10(111101)
             //      = EF BF BD
-            buffer[buffer_offset++] = 0xEF;
-            buffer[buffer_offset++] = 0xBF;
-            buffer[buffer_offset++] = 0xBD;
+            buffer[buffer_offset++] = (char)0xEF;
+            buffer[buffer_offset++] = (char)0xBF;
+            buffer[buffer_offset++] = (char)0xBD;
             offset++;
         }
     }
@@ -158,7 +176,7 @@ utf8_char_iter make_utf8_char_iter(utf8_string ustr) {
 }
 
 bool is_utf8_char_boundary(const char* str) {
-    return (uint8_t)*str <= 0b01111111 || (uint8_t)*str >= 0b11000000;
+    return (uint8_t)*str <= B01111111 || (uint8_t)*str >= B11000000;
 }
 
 utf8_string slice_utf8_string(utf8_string ustr, size_t start_byte_index, size_t byte_len) {
@@ -211,19 +229,19 @@ size_t utf8_char_count(utf8_string ustr) {
 
 uint32_t unicode_code_point(utf8_char uchar) {
     switch (uchar.byte_len) {
-    case 1: return uchar.str[0] & 0b01111111;
+    case 1: return uchar.str[0] & B01111111;
     case 2: return
-        (uchar.str[0] & 0b00011111) << 6 |
-        (uchar.str[1] & 0b00111111);
+        (uchar.str[0] & B00011111) << 6 |
+        (uchar.str[1] & B00111111);
     case 3: return
-        (uchar.str[0] & 0b00001111) << 12 |
-        (uchar.str[1] & 0b00111111) << 6 |
-        (uchar.str[2] & 0b00111111);
+        (uchar.str[0] & B00001111) << 12 |
+        (uchar.str[1] & B00111111) << 6 |
+        (uchar.str[2] & B00111111);
     case 4: return
-        (uchar.str[0] & 0b00000111) << 18 |
-        (uchar.str[1] & 0b00111111) << 12 |
-        (uchar.str[2] & 0b00111111) << 6 |
-        (uchar.str[3] & 0b00111111);
+        (uchar.str[0] & B00000111) << 18 |
+        (uchar.str[1] & B00111111) << 12 |
+        (uchar.str[2] & B00111111) << 6 |
+        (uchar.str[3] & B00111111);
     }
 
     return 0; // unreachable
